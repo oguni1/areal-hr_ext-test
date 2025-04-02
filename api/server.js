264ab3b9b091a3bf7a55cecc5f4ca5b2c74a4954
Ivan <ogarkovjon@gmail.com>
2025-04-02 22:24:09 +0300

    added basic frontend

--- api/server.js
+++ api/server.js
@@ -4,6 +4,16 @@ const cors = require("cors");
 
 const app = express();
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'mydb',
+  password: 'admin',
+  port: 3306,
+});
+
 var corsOptions = {
   origin: "http://localhost:8081"
 };
@@ -19,6 +29,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "test." });
+});
+
+app.get('/organizations', async (req, res) => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM organizations;');
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 // set port, listen for requests
